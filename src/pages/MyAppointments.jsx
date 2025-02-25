a0b3fdc1fbb8bbf4c3a9ee18f41f58493fbda190
morlabhaskar <morlabhaskar306@gmail.com>
2025-02-25 14:50:13 +0530

    for deployment to vercel

--- src/pages/MyAppointments.jsx
+++ src/pages/MyAppointments.jsx
@@ -101,7 +101,25 @@ const MyAppointments = () => {
       </div>
       <h1 className='pb-3 font-bold text-primary border-b text-xl'>My appointments</h1>
       {load ? (
-        <p>Loading...</p>
+        <div className=' min-h-[60vh] flex justify-center items-center'>
+           <svg class="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
+        <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+        <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round" stroke-linejoin="round"
+            stroke-width="24"></line>
+        <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
+        </line>
+        <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
+            stroke-width="24"></line>
+        <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
+        </line>
+        <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
+            stroke-width="24"></line>
+        <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+        <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
+        </line>
+    </svg>
+    <span class="text-4xl font-medium text-gray-500">Loading...</span>
+        </div>
       ) : appointments.length > 0 ? (
         <div className='flex flex-col-reverse md:flex md:flex-row'>
           <div className='md:w-3/4'>
@@ -165,9 +183,9 @@ const MyAppointments = () => {
                     endAngle: 360, // Ensures full circle
 
                     data: [
-                      { id: 0, value: appointments.length, color: "#008080" },
-                      { id: 1, value: completedCount, color: "#3498db" },
-                      { id: 2, value: cancelledCount, color: "#8A2BE2" },
+                      { id: 0, value: appointments.length, color: "#1B0AFC" },
+                      { id: 1, value: completedCount, color: "#15FC0A" },
+                      { id: 2, value: cancelledCount, color: "#FC150A" },
                     ],
                   },
                 ]}
@@ -178,9 +196,9 @@ const MyAppointments = () => {
             </div>
             <div className='flex justify-center'>
               <p className=''>
-                <p className='flex gap-2 items-center text-dar dark:text-whi'><span className='h-[15px] bg-blue-600 w-[3px] px-[8px] '></span><span>Appointments : </span><span>{appointments.length}</span></p>
-                <p className='flex gap-2 items-center text-dar dark:text-whi'><span className='h-[15px] bg-green-500 w-[3px] px-[8px] '></span><span>Completed : </span><span>{completedCount}</span></p>
-                <p className='flex gap-2 items-center text-dar dark:text-whi'><span className='h-[15px] bg-red-600 w-[3px] px-[8px] '></span><span>Cancelled : </span><span>{cancelledCount}</span></p>
+                <p className='flex gap-2 items-center text-dar dark:text-whi'><span className='h-[15px] bg-[#1B0AFC] w-[3px] px-[8px] '></span><span>Appointments : </span><span>{appointments.length}</span></p>
+                <p className='flex gap-2 items-center text-dar dark:text-whi'><span className='h-[15px] bg-[#15FC0A] w-[3px] px-[8px] '></span><span>Completed : </span><span>{completedCount}</span></p>
+                <p className='flex gap-2 items-center text-dar dark:text-whi'><span className='h-[15px] bg-[#FC150A] w-[3px] px-[8px] '></span><span>Cancelled : </span><span>{cancelledCount}</span></p>
               </p>
             </div>
           </div>
